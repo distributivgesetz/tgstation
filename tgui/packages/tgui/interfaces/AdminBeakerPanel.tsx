7edc0b387e1c33f93b032590b8c08e55b2f832d5
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-01 13:57:24 +0200

    basic do nothing panel

---
+++ tgui/packages/tgui/interfaces/AdminBeakerPanel.tsx
@@ -0,0 +1,11 @@
+import { Window } from '../layouts';
+
+export const AdminBeakerPanel = () => {
+  return (
+    <Window width={1100} height={720}>
+      <Window.Content>
+        <div />
+      </Window.Content>
+    </Window>
+  );
+};
